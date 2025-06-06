ecc0f7e90b88aee8ae70ce026d7cc082829306d5
JohnSoi <un.perso@yandex.ru>
2025-06-06 09:56:45 +0500

    Инициализация проекта

---
+++ README.md
@@ -0,0 +1,33 @@
+# Документация DH|Access
+
+## Описание
+
+Данный пакет предназначен для аутентификации и авторизации в платформе DH
+
+## Установка
+
+```commandline
+poetry add git+https://github.com/JohnSoi/dh-access.git
+```
+
+Если планируется активное разработка в пакете, то лучше добавить локальную зависимость:
+```commandline
+poetry add ../dh-access --group dev
+```
+
+
+## Состав
+
+<pre>
+dh_access
+├── models
+└── settings
+</pre>
+
+## Полезные команды 
+
+* Запуск сервера
+
+```commandline
+uvicorn dh_education.main:app --reload
+```
---
+++ dh_access/settings.py
@@ -0,0 +1,61 @@
+"""Модуль базовых настроек"""
+
+__author__: str = "Старков Е.П."
+
+from functools import lru_cache
+
+from pydantic import Extra
+from pydantic_settings import BaseSettings
+
+
+class BaseAppSettings(BaseSettings):
+    """
+    Базовые настройки приложений
+
+    Attributes:
+        APP_ENV (str): Тип окружения
+        DEBUG (bool): Режим отладки
+        PROJECT_NAME (str): Название проекта
+        VERSION (str): Версия проекта
+    Warnings:
+        Данные переменные должны быть описаны в файле .env
+    """
+
+    APP_ENV: str = "production"
+    DEBUG: bool = False
+    PROJECT_NAME: str
+    VERSION: str
+
+    class Config:
+        """Класс конфигурации настроек"""
+
+        env_prefix = "CORE_"
+        env_file = ".env"
+        extra = Extra.ignore
+
+
+@lru_cache
+def get_core_settings() -> BaseAppSettings:
+    """
+    Получение объекта базовых настроек
+
+    Returns:
+        BaseAppSettings: Экземпляр класса базовых настроек
+    Examples:
+        Пример использования в конфиге приложений:
+
+        >>> from pydantic import Field
+        >>> from pydantic_settings import BaseSettings
+        >>> from dh_platform1.settings import get_core_settings
+        >>>
+        >>> class AllSettings(BaseSettings):
+        ...     core: BaseAppSettings = Field(default_factory=get_core_settings)
+        ...
+        ...     class Config:
+        ...         env_nested_delimiter = "__"
+        >>>
+        >>> @lru_cache
+        >>> def get_all_settings() -> AllSettings:
+        ...    return AllSettings()
+    """
+    return BaseAppSettings()
